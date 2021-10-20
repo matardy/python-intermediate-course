97a3e4dcd0afb1fc262010e1b294bbb311f3e03f
Gutemberg S. Mendoza <gutembergmendoza@Gutembergs-MacBook-Pro.local>
2021-10-20 16:44:09 -0500

    Errors

---
+++ errors.py
@@ -0,0 +1,13 @@
+# Syntax error: Detiene el codigo y no lo sigue ejectuando
+
+# Las excepciones se ejectuan en una linea, pero lo anterior
+# si se ejectura en syntax error no sucede asi  
+    # keyboardinterrup: ctrl + c en la consola, eleva una excepcion 
+    # keyError: Llave no existente en un diccionario. 
+    # IndexError: tipico error en listas 
+    # ZeroDivisonError
+    # ImportError 
+
+# Traceback, forma especifica en el que python lee el error
+    # Se debe leer desde el final hasta el principio 
+    
